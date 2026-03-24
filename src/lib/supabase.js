d79ca23610636a00181e95036bfae6299431b68c
AndrewTechEvilLair <lordbizness1234@gmail.com>
2026-03-23 19:33:45 -0700

    move supabase keys to env vars

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://bcbssntzilaethvadbkk.supabase.co'
-const supabaseAnonKey = 'sb_publishable_cfPFjcEQ1Vy8rp-V_RojSQ_YEJyiptC'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
